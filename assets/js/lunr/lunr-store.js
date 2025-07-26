511cc70d4abf8fd5912df1e5c57bb1d26d342d77
rodgersmerokaosumo <rodgersmerokaosumo@users.noreply.github.com>
2025-07-26 12:52:34 +0000

    deploy: 03377976c474271ac8118b00674c88fbc2b8d990

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Hello World",
+        "excerpt":"First post on the new site—checking that the blog works.  ","categories": ["blog"],
+        "tags": [],
+        "url": "/portfolio/blog/2025/07/26/hello-world.html",
+        "teaser": null
+      }]
